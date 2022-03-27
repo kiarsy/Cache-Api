249bc5fb1577db2edb7e710632508008d5bba6af
Hosein Kiarsy Pour <kiarsy@Kia-Laptop.local>
2022-03-27 13:09:54 +0430

    put all together

--- index.ts
+++ index.ts
@@ -1,35 +1,32 @@
-console.log("test")
-
 
 
 import { MongoDatabaseConnection } from './mongo/mongo';
 import cacheEntryModel from './mongo/cacheEntryModel';
+import { WebService } from './webService';
+
+import ReturnAllCachesEndpoint from './endpoints/caches.return.all'
+import ReturnOneCachesEndpoint from './endpoints/caches.return.one'
+import DeleteOneCachesEndpoint from './endpoints/caches.delete.one'
+import DeleteAllCachesEndpoint from './endpoints/caches.delete.all'
+import CreateOneCachesEndpoint from './endpoints/caches.create.one'
+import UpdateOneCachesEndpoint from './endpoints/caches.Update.one'
+
 var mongoConnection = new MongoDatabaseConnection();
+
+const web = new WebService();
+web.addEndPoint(ReturnAllCachesEndpoint);
+web.addEndPoint(ReturnOneCachesEndpoint);
+web.addEndPoint(DeleteOneCachesEndpoint);
+web.addEndPoint(DeleteAllCachesEndpoint);
+web.addEndPoint(CreateOneCachesEndpoint);
+web.addEndPoint(UpdateOneCachesEndpoint);
 
 
 mongoConnection.connect()
     .once("open", async function () {
         console.log("MONGO Connected Successfully.");
-        let x = await cacheEntryModel.find({})
-        console.log(x)
+        web.startWebService()
     })
     .once("error", function (err) {
         console.log("MONGO Connection Error, Please fix this error and run again:", err);
     });
-
-
-import { WebService } from './webService';
-import cachesReturnAll from './endpoints/caches.return.all';
-import cachesCreateOne from './endpoints/caches.create.one';
-import cachesDeleteAll from './endpoints/caches.delete.all';
-import cachesDeleteOne from './endpoints/caches.delete.one';
-import cachesUpdateOne from './endpoints/caches.update.one';
-import cachesReturnOne from './endpoints/caches.return.one';
-const web = new WebService();
-web.addEndPoint(cachesReturnAll)
-web.addEndPoint(cachesCreateOne)
-web.addEndPoint(cachesDeleteAll)
-web.addEndPoint(cachesDeleteOne)
-web.addEndPoint(cachesUpdateOne)
-web.addEndPoint(cachesReturnOne)
-web.startWebService()
